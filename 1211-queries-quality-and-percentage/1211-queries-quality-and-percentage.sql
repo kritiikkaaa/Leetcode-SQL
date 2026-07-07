f2edf80b771b281d3a5495102eb3cc4a98549e84
kritika <155988077+kritiikkaaa@users.noreply.github.com>
2026-07-07 20:51:35 +0530

    Time: 381 ms (50.25%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,5 @@
-SELECT query_name, 
-        ROUND(AVG(rating * 1 / position), 2) AS quality, 
-        ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage
+SELECT query_name,
+        ROUND(AVG(rating / position), 2) AS quality,
+        ROUND(SUM(rating < 3) / count(*) * 100, 2) AS poor_query_percentage
 FROM Queries
-WHERE query_name IS NOT NULL
-GROUP BY query_name;
+GROUP BY query_name
